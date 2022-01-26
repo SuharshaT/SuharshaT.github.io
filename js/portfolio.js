6a8c847041c4129cdfdbd3c6af6e599e0a96e83f
Suharsha <suharsha.27@gmail.com>
2022-01-26 14:58:22 +0530

    sixth commit

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,6 @@
 function navigation(page){
     nextValue = page.id;
+    document.getElementById("Home").textContent = nextValue;
     var active = document.getElementsByClassName("active");
     var currentId = active[0].id;
     active[0].className = "";
@@ -22,6 +23,23 @@ function change(element){
     invisible[0].className = "visible"; 
 }
 
+function myFunction() {
+    var x = document.getElementById("mylinks");
+    var y = document.getElementById("Home");
+    if (y.style.opacity == 0.6){
+        y.style.opacity = 1;
+        y.style.background = "white";
+    } else {
+        y.style.opacity = 0.6;
+        y.style.background = "grey";
+    }
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
 window.onload = unhide;
 
 function unhide(){
@@ -29,9 +47,19 @@ function unhide(){
 }
 
 function hover(element){
-    element.className = "hover";
+    if(element.tagName == "img" || element.tagName == "IMG"){
+        element.classList.add("hoverImage");
+    }
+    else{
+        element.classList.add("hover");
+    }
 }
 
 function leave(element){
-    element.className = "leave";
+    if(element.tagName == "img" || element.tagName == "IMG"){
+        element.classList.remove("hoverImage");
+    }
+    else{
+        element.classList.remove("hover");
+    }
 }
